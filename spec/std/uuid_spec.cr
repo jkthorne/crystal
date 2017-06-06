4988e6ee1f0255969aaa0583dc5f668bec8c2606
Julien Portalier <julien@portalier.com>
2017-06-06 15:22:42 +0200

    Refactor UUID
    - Merged separate files into a single one.
    - Add `new` factories with some raw initializers (from Slice and
      StaticArray).
    - Few optimizations (direct writes to IO, avoid Array allocations).
    - Enhanced specs.

--- spec/std/uuid_spec.cr
+++ spec/std/uuid_spec.cr
@@ -2,60 +2,83 @@ require "spec"
 require "uuid"
 
 describe "UUID" do
-  it "has working zero UUID" do
-    #UUID.empty.should eq UUID.new(UUID::Version::V4, StaticArray(UInt8, 16).new(0_u8))
+  it "returns nil UUID" do
     UUID.empty.to_s.should eq "00000000-0000-0000-0000-000000000000"
     UUID.empty.variant.should eq UUID::Variant::NCS
   end
 
-  it "can be built from strings" do
-    UUID.new("c20335c3-7f46-4126-aae9-f665434ad12b").should eq("c20335c3-7f46-4126-aae9-f665434ad12b")
-    UUID.new("c20335c37f464126aae9f665434ad12b").should eq("c20335c3-7f46-4126-aae9-f665434ad12b")
-    UUID.new("C20335C3-7F46-4126-AAE9-F665434AD12B").should eq("c20335c3-7f46-4126-aae9-f665434ad12b")
-    UUID.new("C20335C37F464126AAE9F665434AD12B").should eq("c20335c3-7f46-4126-aae9-f665434ad12b")
+  it "generates random UUID V4" do
+    uuid = UUID.random
+    uuid.variant.should eq(UUID::Variant::RFC4122)
+    uuid.version.should eq(UUID::Version::V4)
   end
 
-  it "should have correct variant and version" do
-    UUID.new("C20335C37F464126AAE9F665434AD12B").variant.should eq UUID::Variant::RFC4122
-    UUID.new("C20335C37F464126AAE9F665434AD12B").version.should eq UUID::Version::V4
+  it "creates custom UUID" do
+    bytes = StaticArray(UInt8, 16).new { |i| (i + 1).to_u8 }
+    UUID.new(bytes).to_s.should eq("01020304-0506-0708-090a-0b0c0d0e0f10")
   end
 
-  it "supports different string formats" do
-    UUID.new("ee843b2656d8472bb3430b94ed9077ff").to_s.should eq "ee843b26-56d8-472b-b343-0b94ed9077ff"
-    UUID.new("3e806983-eca4-4fc5-b581-f30fb03ec9e5").hexstring.should eq "3e806983eca44fc5b581f30fb03ec9e5"
-    UUID.new("1ed1ee2f-ef9a-4f9c-9615-ab14d8ef2892").urn.should eq "urn:uuid:1ed1ee2f-ef9a-4f9c-9615-ab14d8ef2892"
+  it "creates custom UUID with variant and version" do
+    bytes = StaticArray(UInt8, 16).new { |i| (i + 1).to_u8 }
+    uuid = UUID.new(bytes, UUID::Variant::Microsoft, UUID::Version::V1)
+    uuid.to_s.should eq("01020304-0506-1708-c90a-0b0c0d0e0f10")
+  end
+
+  it "#to_slice" do
+    bytes = StaticArray(UInt8, 16).new { |i| (i + 1).to_u8 }
+    uuid = UUID.new(bytes)
+    uuid.to_slice.should eq(bytes.to_slice)
+  end
+
+  describe "#decode" do
+    it "creates UUID from string" do
+      UUID.new("c20335c3-7f46-4126-aae9-f665434ad12b").should eq("c20335c3-7f46-4126-aae9-f665434ad12b")
+      UUID.new("c20335c37f464126aae9f665434ad12b").should eq("c20335c3-7f46-4126-aae9-f665434ad12b")
+      UUID.new("C20335C3-7F46-4126-AAE9-F665434AD12B").should eq("c20335c3-7f46-4126-aae9-f665434ad12b")
+      UUID.new("C20335C37F464126AAE9F665434AD12B").should eq("c20335c3-7f46-4126-aae9-f665434ad12b")
+    end
+
+    it "decodes variant and version" do
+      uuid = UUID.new("C20335C37F464126AAE9F665434AD12B")
+      uuid.variant.should eq(UUID::Variant::RFC4122)
+      uuid.version.should eq(UUID::Version::V4)
+    end
+
+    it "fails on invalid formats" do
+      expect_raises(ArgumentError) { UUID.new "" }
+      expect_raises(ArgumentError) { UUID.new "25d6f843?cf8e-44fb-9f84-6062419c4330" }
+      expect_raises(ArgumentError) { UUID.new "67dc9e24-0865 474b-9fe7-61445bfea3b5" }
+      expect_raises(ArgumentError) { UUID.new "5942cde5-10d1-416b+85c4-9fc473fa1037" }
+      expect_raises(ArgumentError) { UUID.new "0f02a229-4898-4029-926f=94be5628a7fd" }
+      expect_raises(ArgumentError) { UUID.new "cda08c86-6413-474f-8822-a6646e0fb19G" }
+      expect_raises(ArgumentError) { UUID.new "2b1bfW06368947e59ac07c3ffdaf514c" }
+    end
   end
 
   it "compares to strings" do
-    uuid = UUID.new "c3b46146eb794e18877b4d46a10d1517"
+    uuid = UUID.new("c3b46146eb794e18877b4d46a10d1517")
     uuid.should eq("c3b46146eb794e18877b4d46a10d1517")
     uuid.should eq("c3b46146-eb79-4e18-877b-4d46a10d1517")
     uuid.should eq("C3B46146-EB79-4E18-877B-4D46A10D1517")
     uuid.should eq("urn:uuid:C3B46146-EB79-4E18-877B-4D46A10D1517")
     uuid.should eq("urn:uuid:c3b46146-eb79-4e18-877b-4d46a10d1517")
-    (UUID.new).should_not eq("C3B46146-EB79-4E18-877B-4D46A10D1517")
-  end
 
-  it "fails on invalid arguments when creating" do
-    expect_raises(ArgumentError) { UUID.new "" }
-    expect_raises(ArgumentError) { UUID.new "25d6f843?cf8e-44fb-9f84-6062419c4330" }
-    expect_raises(ArgumentError) { UUID.new "67dc9e24-0865 474b-9fe7-61445bfea3b5" }
-    expect_raises(ArgumentError) { UUID.new "5942cde5-10d1-416b+85c4-9fc473fa1037" }
-    expect_raises(ArgumentError) { UUID.new "0f02a229-4898-4029-926f=94be5628a7fd" }
-    expect_raises(ArgumentError) { UUID.new "cda08c86-6413-474f-8822-a6646e0fb19G" }
-    expect_raises(ArgumentError) { UUID.new "2b1bfW06368947e59ac07c3ffdaf514c" }
+    uuid.should_not eq("A3b46146eb794e18877b4d46a10d1517")
+    uuid.should_not eq("a3b46146eb794e18877b4d46a10d1517")
+    uuid.should_not eq("a3b46146-eb79-4e18-877b-4d46a10d1517")
+    uuid.should_not eq("urn:uuid:a3b46146-eb79-4e18-877b-4d46a10d1517")
   end
 
   it "fails when comparing to invalid strings" do
-    expect_raises(ArgumentError) { UUID.new == "" }
-    expect_raises(ArgumentError) { UUID.new == "d1fb9189-7013-4915-a8b1-07cfc83bca3U" }
-    expect_raises(ArgumentError) { UUID.new == "2ab8ffc8f58749e197eda3e3d14e0 6c" }
-    expect_raises(ArgumentError) { UUID.new == "2ab8ffc8f58749e197eda3e3d14e 06c" }
-    expect_raises(ArgumentError) { UUID.new == "2ab8ffc8f58749e197eda3e3d14e-76c" }
+    expect_raises(ArgumentError) { UUID.random == "" }
+    expect_raises(ArgumentError) { UUID.random == "d1fb9189-7013-4915-a8b1-07cfc83bca3U" }
+    expect_raises(ArgumentError) { UUID.random == "2ab8ffc8f58749e197eda3e3d14e0 6c" }
+    expect_raises(ArgumentError) { UUID.random == "2ab8ffc8f58749e197eda3e3d14e 06c" }
+    expect_raises(ArgumentError) { UUID.random == "2ab8ffc8f58749e197eda3e3d14e-76c" }
   end
 
-  it "should handle variant" do
-    uuid = UUID.new
+  it "handles variant" do
+    uuid = UUID.random
     expect_raises(ArgumentError) { uuid.variant = UUID::Variant::Unknown }
     {% for variant in %w(NCS RFC4122 Microsoft Future) %}
       uuid.variant = UUID::Variant::{{ variant.id }}
@@ -63,12 +86,32 @@ describe "UUID" do
     {% end %}
   end
 
-  it "should handle version" do
-    uuid = UUID.new
+  it "handles version" do
+    uuid = UUID.random
     expect_raises(ArgumentError) { uuid.version = UUID::Version::Unknown }
     {% for version in %w(1 2 3 4 5) %}
       uuid.version = UUID::Version::V{{ version.id }}
       uuid.version.should eq UUID::Version::V{{ version.id }}
+      uuid.v{{ version.id }}?.should be_true
     {% end %}
+  end
+
+  it "formats hyphenated string" do
+    UUID.new("ee843b2656d8472bb3430b94ed9077ff").to_s.should eq("ee843b26-56d8-472b-b343-0b94ed9077ff")
+  end
+
+  it "formats UUID to IO" do
+    uuid = UUID.new("c3b46146eb794e18877b4d46a10d1517")
+    io = IO::Memory.new(36)
+    uuid.to_s(io)
+    io.rewind.to_s.should eq("c3b46146-eb79-4e18-877b-4d46a10d1517")
+  end
+
+  it "formats hexstring" do
+    UUID.new("3e806983-eca4-4fc5-b581-f30fb03ec9e5").hexstring.should eq("3e806983eca44fc5b581f30fb03ec9e5")
+  end
+
+  it "formats URN" do
+    UUID.new("1ed1ee2f-ef9a-4f9c-9615-ab14d8ef2892").urn.should eq("urn:uuid:1ed1ee2f-ef9a-4f9c-9615-ab14d8ef2892")
   end
 end
--- src/uuid.cr
+++ src/uuid.cr
@@ -1,81 +1,225 @@
 require "secure_random"
-require "./uuid/*"
 
 # Universally Unique IDentifier.
 #
-# Supports custom variants with arbitrary 16 bytes as well as (RFC 4122)[https://www.ietf.org/rfc/rfc4122.txt] variant
-# versions.
+# Supports [RFC4122](https://www.ietf.org/rfc/rfc4122.txt) UUIDs and custom
+# variants with arbitrary 16 bytes.
 struct UUID
+  enum Variant
+    # Unknown (ie. custom, your own).
+    Unknown
+
+    # Reserved by the NCS for backward compatibility.
+    NCS
+
+    # As described in the RFC4122 Specification (default).
+    RFC4122
+
+    # Reserved by Microsoft for backward compatibility.
+    Microsoft
+
+    # Reserved for future expansion.
+    Future
+  end
+
+  # RFC4122 UUID variant versions.
+  enum Version
+    # Unknown version.
+    Unknown = 0
+
+    # Version 1 - date-time and MAC address.
+    V1 = 1
+
+    # Version 2 - DCE security.
+    V2 = 2
+
+    # Version 3 - MD5 hash and namespace.
+    V3 = 3
+
+    # Version 4 - random.
+    V4 = 4
+
+    # Version 5 - SHA1 hash and namespace.
+    V5 = 5
+  end
+
+
   # Internal representation.
   @bytes : StaticArray(UInt8, 16)
 
-  # Generates RFC 4122 v4 UUID.
-  def initialize
-    bytes = StaticArray(UInt8, 16).new(0_u8)
-    bytes.to_unsafe.copy_from SecureRandom.random_bytes(16).pointer(16), 16
-    initialize bytes
+  # Generates nil UUID, filled with zero bytes.
+  def self.empty
+    new StaticArray(UInt8, 16).new(0_u8)
   end
 
-  # Creates UUID from 16-`bytes` slice.
-  def initialize(slice : Slice(UInt8))
-    if slice.size != 16
-      raise ArgumentError.new "Invalid bytes length #{@bytes.size}, expected 16."
-    end
-    @bytes = uninitialized UInt8[16]
-    @bytes.to_unsafe.copy_from slice
+  # Generates RFC4122 v4 UUID using a secure random source.
+  def self.random
+    bytes = uninitialized UInt8[16]
+    SecureRandom.random_bytes(bytes.to_slice)
+    new(bytes, Variant::RFC4122, Version::V4)
   end
 
-  # Generates UUID from static 16-`bytes`.
-  def initialize(@bytes : StaticArray(UInt8, 16))
-    initialize @bytes, Version::V4
-  end
-
-  # Creates UUID from string `value`. See `UUID#decode(value : String)` for details on supported string formats.
+  # Decodes UUID from string *value*.
+  #
+  # Supports hyphenated (e.g. `ba714f86-cac6-42c7-8956-bcf5105e1b81`),
+  # hexstring (e.g. `89370a4ab66440c8add39e06f2bb6af6`) or URN (e.g.
+  # `urn:uuid:3f9eaf9e-cdb0-45cc-8ecb-0e5b2bfb0c20`) formats.
   def initialize(value : String)
     @bytes = uninitialized UInt8[16]
-    decode value
+    decode(value)
   end
 
-  # Generates RFC 4122 UUID `variant` with specified `version`.
-  def initialize(@bytes : StaticArray(UInt8, 16), version = Version::V4)
-    #@bytes.to_unsafe.copy_from SecureRandom.random_bytes(16).pointer(16), 16
+  # Creates UUID from 16-bytes slice.
+  def initialize(slice : Slice(UInt8))
+    raise ArgumentError.new "Invalid bytes length #{@bytes.size}, expected 16." unless slice.size == 16
+    @bytes = uninitialized UInt8[16]
+    @bytes.to_unsafe.copy_from(slice)
+  end
 
-    case version
-    when Version::V4
-      variant = Variant::RFC4122
-      version = Version::V4
+  # Creates UUID from 16-bytes static array.
+  def initialize(@bytes)
+  end
+
+  # Creates UUID from bytes, applying *version* and *variant* to the UUID.
+  def initialize(@bytes, variant : Variant, version : Version)
+    self.variant = variant
+    self.version = version
+  end
+
+  delegate to_slice, to: @bytes
+  delegate to_unsafe, to: @bytes
+
+  def variant
+    case
+    when @bytes[8] & 0x80 == 0x00
+      Variant::NCS
+    when @bytes[8] & 0xc0 == 0x80
+      Variant::RFC4122
+    when @bytes[8] & 0xe0 == 0xc0
+      Variant::Microsoft
+    when @bytes[8] & 0xe0 == 0xe0
+      Variant::Future
     else
-      raise ArgumentError.new "Creating #{version} not supported."
+      Variant::Unknown
     end
   end
 
-  # Returns 16-byte slice.
-  def to_slice
-    Slice(UInt8).new to_unsafe, 16
+  def variant=(value : Variant)
+    case value
+    when Variant::NCS
+      @bytes[8] = (@bytes[8] & 0x7f)
+    when Variant::RFC4122
+      @bytes[8] = (@bytes[8] & 0x3f) | 0x80
+    when Variant::Microsoft
+      @bytes[8] = (@bytes[8] & 0x1f) | 0xc0
+    when Variant::Future
+      @bytes[8] = (@bytes[8] & 0x1f) | 0xe0
+    else
+      raise ArgumentError.new "Can't set unknown variant."
+    end
   end
 
-  # Returns unsafe pointer to 16-bytes.
-  def to_unsafe
-    @bytes.to_unsafe
+  # Returns version based on RFC4122 format. See also `#variant`.
+  def version
+    case @bytes[6] >> 4
+    when 1 then Version::V1
+    when 2 then Version::V2
+    when 3 then Version::V3
+    when 4 then Version::V4
+    when 5 then Version::V5
+    else        Version::Unknown
+    end
   end
 
-  # Writes hyphenated format string to the *io*.
-  def to_s(io : IO)
-    io << to_s
+  # Sets `version`. Doesn't set variant (see `#variant=`).
+  def version=(value : Version)
+    raise ArgumentError.new "Can't set unknown version." if value.unknown?
+    @bytes[6] = (@bytes[6] & 0xf) | (value.to_u8 << 4)
   end
 
-  # Returns `true` if `other` string represents the same UUID, `false` otherwise.
+  {% for v in %w(1 2 3 4 5) %}
+    # Returns true if UUID is a `Version::V{{ v.id }}`, false otherwise.
+    def v{{ v.id }}?
+      variant == Variant::RFC4122 && version == Version::V{{ v.id }}
+    end
+
+    # Raises `Error` unless UUID is a `Version::V{{ v.id }}`.
+    def v{{ v.id }}!
+      unless v{{ v.id }}?
+        raise Error.new("Invalid UUID variant #{variant} version #{version}, expected RFC4122 V{{ v.id }}.")
+      end
+    end
+  {% end %}
+
   def ==(other : String)
-    self == UUID.new other
+    self == UUID.new(other)
   end
 
-  # Returns `true` if `other` 16-byte slice represents the same UUID, `false` otherwise.
   def ==(other : Slice(UInt8))
     to_slice == other
   end
 
-  # Returns `true` if `other` static 16 bytes represent the same UUID, `false` otherwise.
   def ==(other : StaticArray(UInt8, 16))
-    self.==(Slice(UInt8).new other.to_unsafe, 16)
+    @bytes == other
+  end
+
+  protected def decode(value : String)
+    case value.size
+    when 36 # hyphenated
+      8.step(to: 23, by: 5) do |offset|
+        raise ArgumentError.new "Invalid UUID string format, expected hyphen at char #{offset}." unless value[offset] == '-'
+      end
+      {0, 2, 4, 6, 9, 11, 14, 16, 19, 21, 24, 26, 28, 30, 32, 34}.each_with_index do |offset, i|
+        @bytes[i] = value[offset, 2].to_u8(16, whitespace: false, underscore: false, prefix: false)
+      end
+    when 32 # hexstring
+      16.times do |i|
+        @bytes[i] = value[i * 2, 2].to_u8(16, whitespace: false, underscore: false, prefix: false)
+      end
+    when 45 # URN
+      raise ArgumentError.new "Invalid URN UUID format, expected string starting with \":urn:uuid:\"." unless value.starts_with?("urn:uuid:")
+      {9, 11, 13, 15, 18, 20, 23, 25, 28, 30, 33, 35, 37, 39, 41, 43}.each_with_index do |offset, i|
+        @bytes[i] = value[offset, 2].to_u8(16, whitespace: false, underscore: false, prefix: false)
+      end
+    else
+      raise ArgumentError.new "Invalid string length #{value.size} for UUID, expected 32 (hexstring), 36 (hyphenated) or 46 (urn)."
+    end
+  end
+
+  # Writes hyphenated representation of UUID to *io*.
+  def to_s(io : IO)
+    @bytes.each_with_index do |byte, i|
+      io << '0' if byte < 16
+      byte.to_s(16, io)
+      io << '-' if i == 3 || i == 5 || i == 7 || i == 9
+    end
+  end
+
+  def to_s
+    String.new(36) do |buffer|
+      format_hyphenated(buffer)
+      {36, 36}
+    end
+  end
+
+  def hexstring
+    to_slice.hexstring
+  end
+
+  def urn
+    String.new(45) do |buffer|
+      buffer.copy_from("urn:uuid:".to_unsafe, 9)
+      format_hyphenated(buffer + 9)
+      {45, 45}
+    end
+  end
+
+  protected def format_hyphenated(buffer)
+    buffer[8] = buffer[13] = buffer[18] = buffer[23] = 45_u8
+    to_slice[0, 4].hexstring(buffer + 0)
+    to_slice[4, 2].hexstring(buffer + 9)
+    to_slice[6, 2].hexstring(buffer + 14)
+    to_slice[8, 2].hexstring(buffer + 19)
+    to_slice[10, 6].hexstring(buffer + 24)
   end
 end
