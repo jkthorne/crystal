32687b0e7c8ed354b91931004a32d9da03f63be4
Jack Thorne <jack@kilmer.co>
2022-01-21 02:03:10 -0600

    working on specs

--- spec/std/xml/serializable_spec.cr
+++ spec/std/xml/serializable_spec.cr
@@ -1,5 +1,6 @@
 require "../spec_helper"
 require "xml"
+require "uuid"
 
 record XMLAttrPoint, x : Int32, y : Int32 do
   include XML::Serializable
@@ -54,6 +55,52 @@ class XMLAttrPersonExtraFields
 
   property name : String
   property age : Int32?
+end
+
+class XMLAttrPersonEmittingNull
+  include XML::Serializable
+
+  property name : String
+
+  @[XML::Element(emit_null: true)]
+  property age : Int32?
+end
+
+@[XML::Serializable::Options(emit_nulls: true)]
+class XMLAttrPersonEmittingNullsByOptions
+  include XML::Serializable
+
+  property name : String
+  property age : Int32?
+  property value1 : Int32?
+
+  @[XML::Element(emit_null: false)]
+  property value2 : Int32?
+end
+
+class XMLAttrWithBool
+  include XML::Serializable
+
+  property value : Bool
+end
+
+class XMLAttrWithUUID
+  include XML::Serializable
+
+  property value : UUID
+end
+
+# class XMLAttrWithBigDecimal
+#   include XML::Serializable
+
+#   property value : BigDecimal
+# end
+
+class XMLAttrWithTime
+  include XML::Serializable
+
+  @[XML::Element(converter: Time::Format.new("%F %T"))]
+  property value : Time
 end
 
 describe "XML mapping" do
@@ -186,7 +233,7 @@ describe "XML mapping" do
     person.age.should eq(30)
   end
 
-  it "parses strict person with unknown attributes" do
+  pending "parses strict person with unknown attributes" do
     xml = <<-XML
       <?xml version="1.0"?>
       <XMLAttrPerson>
@@ -207,7 +254,7 @@ describe "XML mapping" do
     # ex.location.should eq({4, 3}) TODO: implement location
   end
 
-  it "should parse extra fields (XMLAttrPersonExtraFields with on_unknown_json_attribute)" do
+  pending "should parse extra fields (XMLAttrPersonExtraFields with on_unknown_xml_attribute)" do
     xml = <<-XML
       <?xml version="1.0"?>
       <XMLAttrPersonExtraFields>
@@ -224,5 +271,175 @@ describe "XML mapping" do
     person.age.should eq(30)
     # TODO: "z" => [1, 2, 3]
     person.xml_unmapped.should eq({"x" => "1", "y" => 2_i64})
+  end
+
+  pending "should to store extra fields (XMLAttrPersonExtraFields with on_to_xml)" do
+    original_xml = <<-XML
+      <?xml version="1.0"?>
+      <XMLAttrPersonExtraFields>
+        <name>John</name>
+        <age>30</age>
+        <x>1</x>
+        <y>2</y>
+      </XMLAttrPersonExtraFields>\n
+      XML
+    # TODO: <z>1,2,3</z>
+    expected_xml = <<-XML
+      <?xml version="1.0"?>
+      <XMLAttrPersonExtraFields>
+        <name>John1</name>
+        <age>30</age>
+        <x>1</x>
+        <y>2</y>
+        <q>w</q>
+      </XMLAttrPersonExtraFields>\n
+      XML
+
+    person = XMLAttrPersonExtraFields.from_xml(original_xml)
+    person.name = "John1"
+    person.xml_unmapped.delete("y")
+    person.xml_unmapped["q"] = XML::Any.new("w")
+    # TODO: "z" => [1, 2, 3]
+    person.to_xml.should eq expected_xml
+  end
+
+  pending "raises if non-nilable attribute is nil" do
+    xml = <<-XML
+      <?xml version="1.0"?>
+      <XMLAttrPerson>
+        <age>30</age>
+      </XMLAttrPerson>\n
+      XML
+
+    error_message = <<-'MSG'
+      Missing XML attribute: name
+        parsing XMLAttrPerson at line 1, column 1
+      MSG
+
+    ex = expect_raises ::XML::SerializableError, error_message do
+      XMLAttrPerson.from_xml(xml)
+    end
+    # TODO: ex.location.should eq({1, 1})
+  end
+
+  pending "raises if not an object" do
+    error_message = <<-'MSG'
+      Expected BeginObject but was String at line 1, column 1
+        parsing StrictXMLAttrPerson at line 0, column 0
+      MSG
+    ex = expect_raises ::XML::SerializableError, error_message do
+      StrictXMLAttrPerson.from_xml <<-XML
+        "foo"
+        XML
+    end
+    # ex.location.should eq({1, 1})
+  end
+
+  pending "raises if data type does not match" do
+    xml = <<-XML
+      <?xml version="1.0"?>
+      <XMLAttrPerson>
+        <name>John</name>
+        <age>30</age>
+        <foo>bar</foo>
+      </XMLAttrPerson>\n
+      XML
+
+    error_message = <<-MSG
+      Couldn't parse (Int32 | Nil) from "foo" at line 3, column 10
+      MSG
+    ex = expect_raises ::XML::SerializableError, error_message do
+      StrictXMLAttrPerson.from_xml xml
+    end
+    # ex.location.should eq({3, 10})
+  end
+
+  pending "doesn't emit null by default when doing to_xml" do
+    xml = <<-XML
+      <?xml version="1.0"?>
+      <XMLAttrPerson>
+        <name>John</name>
+      </XMLAttrPerson>\n
+      XML
+
+    person = XMLAttrPerson.from_xml(xml)
+    (person.to_xml =~ /age/).should be_falsey
+  end
+
+  it "emits null on request when doing to_xml" do
+    xml = <<-XML
+      <?xml version="1.0"?>
+      <XMLAttrPersonEmittingNull>
+        <name>John</name>
+      </XMLAttrPersonEmittingNull>\n
+      XML
+
+    person = XMLAttrPersonEmittingNull.from_xml(xml)
+    (person.to_xml =~ /age/).should be_truthy
+  end
+
+  it "emit_nulls option" do
+    original_xml = <<-XML
+      <?xml version="1.0"?>
+      <XMLAttrPerson>
+        <name>John</name>
+      </XMLAttrPerson>\n
+      XML
+
+    expected_xml = String.build do |str|
+      str << "<?xml version=\"1.0\"?>\n"
+      str << "<XMLAttrPersonEmittingNullsByOptions>"
+      str << "<name>John</name><age/><value1/><value2/>"
+      str << "</XMLAttrPersonEmittingNullsByOptions>\n"
+    end
+
+    person = XMLAttrPersonEmittingNullsByOptions.from_xml(original_xml)
+    person.to_xml.should eq expected_xml
+  end
+
+  it "doesn't raises on false value when not-nil" do
+    xml = <<-XML
+      <?xml version="1.0"?>
+      <XMLAttrWithBool>
+        <value>false</value>
+      </XMLAttrWithBool>\n
+      XML
+
+    xml = XMLAttrWithBool.from_xml(xml)
+    xml.value.should be_false
+  end
+
+  it "parses UUID" do
+    xml = <<-XML
+      <?xml version="1.0"?>
+      <XMLAttrWithBool>
+        <value>ba714f86-cac6-42c7-8956-bcf5105e1b81</value>
+      </XMLAttrWithBool>\n
+      XML
+
+    uuid = XMLAttrWithUUID.from_xml(xml)
+    uuid.should be_a(XMLAttrWithUUID)
+    uuid.value.should eq(UUID.new("ba714f86-cac6-42c7-8956-bcf5105e1b81"))
+  end
+
+  it "parses xml with Time::Format converter" do
+    original_xml = <<-XML
+      <?xml version="1.0"?>
+      <XMLAttrWithTime>
+        <value>2014-10-31 23:37:16</value>
+      </XMLAttrWithTime>\n
+      XML
+
+    expected_xml = <<-XML
+      <?xml version="1.0"?>
+      <XMLAttrWithTime>
+        <value>2014-10-31 23:37:16</value>
+      </XMLAttrWithTime>\n
+      XML
+
+    xml = XMLAttrWithTime.from_xml(original_xml)
+    xml.value.should be_a(Time)
+    xml.value.to_s.should eq("2014-10-31 23:37:16 UTC")
+    xml.to_xml.should eq(expected_xml)
   end
 end
